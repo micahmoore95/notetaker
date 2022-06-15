8b5ef6eece411a3eff8772e1f5e9d01c3946a5cb
Moore <mooreart95@gmail.com>
2022-06-14 19:19:43 -0600

    get and post routes

---
+++ routes/apiRoutes/noteRoutes.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const router = require("express").Router();
+const notes = require("../../db/db.json");
+const generateUniqueId = require("generate-unique-id");
+const path = require("path");
+
+//API GET request
+router.get("/notes", (req, res) => {
+  res.status(200).json(notes);
+});
+
+//API POST request
+router.post("/notes", (req, res) => {
+  const newNote = req.body;
+  const id = generateUniqueId();
+  newNote.id = id;
+  notes.push(newNote);
+  fs.writeFileSync(
+    path.join(__dirname, "../../db/db.json"),
+    JSON.stringify(notes)
+  );
+  res.status(200).send("New note created!");
+});
+
+module.exports = router;
